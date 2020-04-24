cf936e2e46f948b1224c6344045267a638b7eea8
Natallia198 <64219705+Natallia198@users.noreply.github.com>
2020-04-24 16:59:37 -0400

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -3,8 +3,7 @@ package day_1;
 public class AllGood {
     public static void main(String[] args) {
         //i hope everyone is good
-        //this line is added from GitHub
-        //helooooooo
+        //this line from GitHub
 
     }
 }
