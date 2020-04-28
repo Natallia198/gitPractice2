2209e8c6f0ff341ebfb954c8ec0ed063fb60150a
Natallia198 <64219705+Natallia198@users.noreply.github.com>
2020-04-28 15:49:58 -0400

    Update TodayIsAGreatDAy.java

--- src/day_1/TodayIsAGreatDAy.java
+++ src/day_1/TodayIsAGreatDAy.java
@@ -3,5 +3,7 @@ package day_1;
 public class TodayIsAGreatDAy {
     public static void main(String[] args) {
         System.out.println("Today is a great day");
+        System.out.println("Today is a great day");
+       
     }
 }
